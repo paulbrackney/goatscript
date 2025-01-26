cc087a2d1b43dc7d6edebb34fa20449ff1f96a66
Paul Brackney <paulbrack2000@gmail.com>
2025-01-26 11:27:29 -0600

    add goatscript title and table

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import criblLogo from './assets/cribl-logo.png'
 import './App.css'
 
 function App() {
@@ -9,14 +10,60 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://cribl.io" target="_blank">
+          <img src={criblLogo} className="logo" alt="Cribl logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>GoatScript</h1>
+      <div className="card">
+        <p>
+          GoatScript is the first and only programming language for goats!
+          &#128016; &#128016; &#128016;
+        </p>
+        <p>
+          Here's a quick breakdown:
+        </p>
+        <ul>
+          <li>
+            <p>
+            Common keywords are replaced with goat-related terms
+            </p>
+            <table>
+          <tr>
+            <th>JavaScript</th>
+            <th>GoatScript</th>
+          </tr>
+          <tr>
+            <td>Array</td>
+            <td>Herd</td>
+          </tr>
+          <tr>
+            <td>console.log</td>
+            <td>stable.say</td>
+          </tr>
+          <tr>
+            <td>function</td>
+            <td>goat</td>
+          </tr>
+          <tr>
+            <td>let</td>
+            <td>bleat</td>
+          </tr>
+          <tr>
+            <td>true</td>
+            <td>yes</td>
+          </tr>
+          <tr>
+            <td>false</td>
+            <td>no</td>
+          </tr>
+        </table>
+          </li>
+        </ul>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
